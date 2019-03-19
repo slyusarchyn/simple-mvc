6a08349f9ef2420560a443e410a9aca12941bd32
Alexander Slyusarchyn <sashaslyusarchyn@gmail.com>
2019-03-19 06:36:51 +0200

    Added style (app.css) file

--- views/layout/app.php
+++ views/layout/app.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <title>Обо мне</title>
         <meta name="viewport" content="width=device-width,initial-scale=1">
-        <link href="/Css/style_layout.css" rel="stylesheet" type="text/css">
+        <link href="/css/app.css" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
     </head>
     <body>
